3504c92dfa0967a341aafaf0d98827b1914c056f
noskill <noskill@fixme.ch>
2015-10-11 11:52:05 +0200

    Adds the setup.py script for easy installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        url='noskill.me/homePie'
+        name='HomePie'
+        version=0.1
+        author='Marc Savioz (aka noskill)'
+        author_email='noskill@fixme.ch'
+        description=('Sutomation software using Nordic Semiconductors sensor modules'
+                     'and a raspberry pi')
+        )
